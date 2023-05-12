d2ebfc1c9d836667fe01dbe75229f11a91bb71f6
NickLammerskitten <120336345+NickLammerskitten@users.noreply.github.com>
2023-05-12 16:43:58 +0200

    10-datenbank-erstellen - hinzufügen von Database connection mit insert Funktion

--- src/verwaltung/strafeHinzufuegen.php
+++ src/verwaltung/strafeHinzufuegen.php
@@ -1,1 +1,22 @@
 <?php
+$servername = "localhost";
+$username = "root";
+$password = "root";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=strafenkatalog", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $schuelerId = $_REQUEST['schueler'];
+    $strafeId = $_REQUEST['strafNummer'];
+    $datumErfassung = date("Y-m-d");
+
+    $sql = "INSERT INTO t_HatStrafe (FKSchuelerId, FKStrafeId, DatumErfassung) 
+                VALUES ('$schuelerId' , '$strafeId', '$datumErfassung');";
+
+    $conn->query($sql);
+    echo "New record created successfully";
+} catch(PDOException $e) {
+    echo "<br>" . $e->getMessage();
+}
